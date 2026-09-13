31826be738ebcad03b0c54ccb40246b7dfc57ee4
JP Miranda <jpmiranda103@gmail.com>
2026-09-12 22:15:52 -0600

    renamed about and award section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import  Hero  from '@/components/Hero';
-import { AboutSection } from "@/components/About-section";
-import AwardSection from "@/components/award-section";
+import  AboutSection from "@/components/AboutSection";
+import AwardSection from "@/components/AwardSection";
 import GoogleCalendar from "@/components/Google-calendar";
 
 export default function Home() {
